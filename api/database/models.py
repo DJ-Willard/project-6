26bdb671ad5337968a84c8f56e1538581035d36b
root <root@GearBoy>
2023-03-11 20:55:41 -0800

    from lab 3/10 talk updated models

--- api/database/models.py
+++ api/database/models.py
@@ -1,27 +1,30 @@
 from mongoengine import *
 
 
-class Checkpoint(EmbeddedDocument):
+class Control_List(EmbeddedDocument):
     """
+    Disregard schema and datetime for lab 3/10
     A MongoEngine EmbeddedDocument containing:
         distance: MongoEngine float field, required, (checkpoint distance in kilometers),
 		location: MongoEngine string field, optional, (checkpoint location name),
 		open_time: MongoEngine datetime field, required, (checkpoint opening time),
 		close_time: MongoEngine datetime field, required, (checkpoint closing time).
     """
-    distance = FloatField(required=True)
-    location = StringField()
-    open_time = DateTimeField(required=True)
-    close_time = DateTimeField(required=True)
+    miles  = FloatField()
+    km  = FloatField(required=True)
+    loc = StringField()
+    open_t= StringField(required=True)
+    close_t = StringField(required=True)
 
 
 class Brevet(Document):
-    """
+    """ 
+    Disregard schema and datetime for lab 3/10
     A MongoEngine document containing:
 		length: MongoEngine float field, required
 		start_time: MongoEngine datetime field, required
 		checkpoints: MongoEngine list field of Checkpoints, required
     """
-    length = FloatField(required=True)
-    start_time = DateTimeField(require=True)
-    checkpoints = EmbeddedDocumentListField(Checkpoint)
+    brevet_dist = FloatField(required=True)
+    start_time = StringField(require=True)
+    control_list = EmbeddedDocumentListField(Control_List)
